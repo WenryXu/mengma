2ec255d93d4f9b0560686d2502f6a6f5798ad2d8
WenryXu <wenryxu@outlook.com>
2015-09-20 01:37:44 +0800

    Add assert

---
+++ assert/js/main.js
@@ -0,0 +1,27 @@
+/**
+ * Created by WenryXu on 15/2/4.
+ */
+
+$('.ascii').one('mouseover', function () {
+    $(this).zclip({
+        path:  'assert/js/ZeroClipboard.swf',
+        copy: function () {
+            return $(this).html();
+        },
+        beforeCopy: null,
+        afterCopy: null,
+        clickAfter: true,
+        setHandCursor: true,
+        setCSSEffects: true
+    });
+});
+
+$('.ascii').on('mouseout', function () {
+    $(this).css('color', '');
+    $(this).css('margin-left', '');
+});
+
+$('.ascii').on('mouseenter', function () {
+    $(this).css('color', '#f39c12');
+    $(this).css('margin-left', '-5px');
+});
